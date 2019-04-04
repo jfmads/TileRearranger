29d0890cdca5d2db12e3d17cbf9a09b3324fae68
Joey Madden <48923144+jfmads@users.noreply.github.com>
2019-04-03 20:37:00 -0400

    Init Commit

---
+++ Madden_Search/TileFix.fs
@@ -0,0 +1,125 @@
+﻿// Joseph Madden
+
+open TreeSearch
+open System
+
+type State = char []
+
+let goalState = [|  'L'; 'L'; 'L'; ' '; 'R'; 'R'; 'R' |]
+
+let startState0 = [|  'L'; 'L'; 'L'; 'R'; ' '; 'R'; 'R' |]
+let startState1 = [|  'L';  'R'; ' '; 'L';  'L'; 'R';'R' |]
+let startState2 = [|  'L';  'R'; 'R';   'L';  'L'; 'R'; ' ' |] 
+let startState3 = [| 'R'; 'R'; ' ';  'L'; 'L'; 'R'; 'L' |] 
+let startState4 = [| 'R'; 'R'; 'R'; ' '; 'L'; 'L'; 'L' |]
+
+let startState = startState2
+
+let goalTest s = s = goalState
+
+let copyState (s:State) =
+    let newState:State = [| '.'; '.'; '.'; '.'; '.'; '.'; '.' |]
+    for i in 0..6 do
+        newState.[i] <- s.[i]
+    newState
+
+let isGoalState (s:State) =
+    let mutable ans = true
+    for i in 0..6 do
+        if not (s.[i] = goalState.[i]) then
+            ans <- false
+    ans
+    
+let findBlank (st:State) =
+    let mutable spot = 0
+    for idx in 0..6 do
+        if st.[idx] = ' ' then
+            spot <- idx
+    spot
+       
+// actions
+let slideLeft (st:State) =
+    let s = copyState st
+    let blank = findBlank s
+    if blank >= 1 then
+        let moved = s.[blank-1]
+        s.[blank] <- moved
+        s.[blank-1] <- ' '
+    s
+    
+let hopLeft (st:State) = 
+    let s = copyState st
+    let blank = findBlank s
+    if blank >= 2 then
+        let moved = s.[blank-2]
+        s.[blank] <- moved
+        s.[blank-2] <- ' ' 
+    s
+
+let hop2Left (st:State) =   
+    let s = copyState st
+    let blank = findBlank s
+    if blank >= 3 then
+        let moved = s.[blank-3]
+        s.[blank] <- moved
+        s.[blank-3] <- ' ' 
+    s
+
+let slideRight (st:State) = 
+    let s = copyState st
+    let blank = findBlank s
+    if blank <= 5 then
+        let moved = s.[blank+1]
+        s.[blank] <- moved 
+        s.[blank+1] <- ' '
+    s
+
+let hopRight (st:State) = 
+    let s = copyState st
+    let blank = findBlank s
+    if blank <= 4 then
+        let moved = s.[blank + 2]
+        s.[blank] <- moved
+        s.[blank + 2] <- ' ' 
+    s
+
+let hop2Right (st:State) = 
+    let s = copyState st
+    let blank = findBlank s
+    if blank <= 3 then
+        let moved = s.[blank+3]
+        s.[blank] <- moved
+        s.[blank+3] <- ' '
+    s
+
+let actions = [| slideLeft; hopLeft; hop2Left; slideRight; hopRight; hop2Right; |]
+let names = [| "slideLeft"; "hopLeft"; "hop2Left"; "slideRight"; "hopRight"; "hop2Right"; |] 
+let costs = [| 2.0; 3.0; 4.0; 2.0; 3.0; 4.0;|]
+
+let heuristic (st:State) =
+    let mutable tilesWrong = 0
+    let s = copyState st
+    for i in 0..6 do
+       if not (s.[i] = goalState.[i]) then
+            tilesWrong <- tilesWrong + 1
+    (double tilesWrong)
+
+let problem = { Start = startState;
+                IsGoal = goalTest;
+                Actions = actions;
+                Names = names;
+                Costs = costs; 
+                Heuristic = heuristic;}
+
+let startTime = DateTime.Now
+let (satisfied,g) = ucs problem 
+let finishTime = DateTime.Now
+let elapsed = finishTime - startTime
+
+printfn "%A" g
+printfn "with %d steps" g.Depth
+printfn "%d nodes were expanded" nodesExpanded
+printfn "Took time: %A" elapsed
+
+System.Console.ReadLine() |> ignore
+
---
+++ Madden_Search/TreeSearch.fs
@@ -0,0 +1,106 @@
+﻿// Joseph Madden
+module TreeSearch
+
+type Problem<'State> = 
+    { 
+      Start : 'State; 
+      IsGoal : 'State -> bool; 
+      Heuristic : 'State -> double;
+      Actions : array<'State -> 'State>; 
+      Names : array<string>
+      Costs : array<double>
+    }
+
+type Node<'State> =
+    { 
+      State : 'State;
+      Parent : 'State;
+      Action : int;
+      Depth : int;
+      Cost : double
+      Value : double
+    }
+
+let mutable nodesExpanded = 0
+
+let expand problem node =
+    nodesExpanded <- nodesExpanded + 1
+    [ for i = 0 to problem.Actions.Length-1 do 
+        let action = problem.Actions.[i]
+        yield { State = action node.State;
+                Parent = node.State;
+                Action = i;
+                Depth = node.Depth + 1
+                Cost = node.Cost + problem.Costs.[i];
+                Value = 0.0 }
+    ]
+
+let treeSearch problem combiner =
+    //nodesExpanded <- 0
+    let start = { State = problem.Start;
+                  Parent = problem.Start;
+                  Action = -1;  // not a valid array index - no action
+                  Depth = 0;
+                  Cost = 0.0;
+                  Value = 0.0 }
+
+    let mutable frontier = [start]
+    let mutable goalSatisfied = false
+    let mutable currentNode = start
+
+    while (not (List.isEmpty frontier)) && (not goalSatisfied) do
+        currentNode <- frontier.Head
+       // printf "%A" currentNode.State
+        if problem.IsGoal currentNode.State then
+            goalSatisfied <- true
+        else
+            frontier <- combiner (expand problem frontier.Head) frontier.Tail
+    (goalSatisfied, currentNode)
+    
+let dfs problem = treeSearch problem List.append
+
+(* let bfs problem = 
+    let prepend children old = old @ children
+    treeSearch problem prepend *)
+
+let bfs problem = treeSearch problem (fun l1 l2 -> l2 @ l1)
+
+let dls problem limit =
+    let dlsCombine l1 l2 = (l1 @ l2) |> List.filter (fun n -> n.Depth <= limit)
+    treeSearch problem dlsCombine
+
+let ids problem = 
+    let mutable limit = 1
+    let mutable result = dls problem limit
+    while fst result |> not do
+        limit <- limit + 1
+        result <- dls problem limit
+    result
+
+let ucs problem =
+   let ucCombine children old = (children @ old) |> List.sortBy (fun n -> n.Cost)
+   treeSearch problem ucCombine
+
+let gbfs problem =
+    let gbfCombine children old =
+        let newChildren = [for n in children -> { n with Value = problem.Heuristic n.State }]
+        (newChildren @ old) |> List.sortBy (fun n -> n.Value )
+    treeSearch problem gbfCombine
+
+let asts problem =
+    let astCombine children old =
+        let newChildren = [for n in children -> { n with Value = n.Cost + problem.Heuristic n.State }]
+        (newChildren @ old) |> List.sortBy (fun n -> n.Value )
+    treeSearch problem astCombine
+    
+let fls problem limit = 
+    let flsCombine l1 l2 = (l1 @ l2) |> List.filter (fun n -> n.Cost + problem.Heuristic n.State <= limit)
+    treeSearch problem flsCombine
+    
+let idas problem =
+    let mutable limit = 1.0
+    let mutable result = fls problem limit
+    while fst result |> not do
+        limit <- limit + 1.0
+        result <- fls problem limit
+    result
